834634fdc8ed3840624907b7a3b78de52b2c9bc2
Shreyansh Jain <jshreyansh962@gmail.com>
2026-03-16 20:56:59 +0530

    error fixes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwSB7HoEPd190sFDLqTGGgu2W92mIvDm4",
-  authDomain: "ecotwinai-1.firebaseapp.com",
-  projectId: "ecotwinai-1",
-  storageBucket: "ecotwinai-1.firebasestorage.app",
-  messagingSenderId: "112553663751",
-  appId: "1:112553663751:web:65ce100b296005dc8f79a2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
